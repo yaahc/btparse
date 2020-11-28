840221e4178001829afe05f1a773fe0d6aa66e10
Jane Lusby <jlusby@yaah.dev>
2020-11-27 16:51:25 -0800

    checkpoint2

--- test_coverage.sh
+++ test_coverage.sh
@@ -11,7 +11,7 @@ git push || true
 $(rustc --print target-libdir)/../bin/llvm-profdata merge --sparse ./target/debug/coverage/test.*.profraw -o ./target/test.profdata
 
 # This one works and shows all the details I want in the CLI
-$(rustc --print target-libdir)/../bin/llvm-cov show -format=html -Xdemangler=rustfilt -instr-profile=./target/test.profdata $(find target/debug/deps -type f -perm -u+x ! -name '*.so') -show-regions -show-instantiations
+$(rustc --print target-libdir)/../bin/llvm-cov show -format=html -Xdemangler=rustfilt -instr-profile=./target/test.profdata $(find target/debug/deps -type f -perm -u+x ! -name '*.so') -show-regions -show-instantiations > index.html
 
 # This one gives an error indicating that there was an error parsing the report
 $(rustc --print target-libdir)/../bin/llvm-cov show -Xdemangler=rustfilt -instr-profile=./target/test.profdata $(find target/debug/deps -type f -perm -u+x ! -name '*.so') -show-regions -show-instantiations > coverage.txt
